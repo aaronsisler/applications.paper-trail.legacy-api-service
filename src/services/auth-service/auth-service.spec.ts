fe808d41c3b5667afd3eed93f8d76d0b84de0fcd
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-08-09 15:45:13 -0400

    Updating the auth spec for full coverage

--- src/services/auth-service/auth-service.spec.ts
+++ src/services/auth-service/auth-service.spec.ts
@@ -1,8 +1,18 @@
+import axios from "axios";
 import { AuthService } from "./index";
+
+jest.mock("../../config", () => ({
+  TOKEN_HEADER: "mock-token-header",
+  TOKEN_VALIDATION_URL: "mock-token-validation-url"
+}));
+
+jest.mock("axios", () => ({ get: jest.fn() }));
 
 describe("AuthService", () => {
   let authService: AuthService;
   let consoleLog: any;
+  let returnedAuthId: string;
+  const mockRequest = { headers: { "mock-token-header": "mock-token" } };
 
   beforeEach(() => {
     consoleLog = console.log;
@@ -17,5 +27,50 @@ describe("AuthService", () => {
   it("should be a class", () => {
     expect(typeof AuthService).toEqual("function");
     expect(typeof authService).toEqual("object");
+  });
+
+  describe("when getAuthId is invoked", () => {
+    describe("and is successful", () => {
+      beforeEach(async () => {
+        axios.get = jest.fn().mockResolvedValue({ data: { sub: "mock-sub" } });
+        authService = new AuthService();
+        returnedAuthId = await authService.getAuthId(mockRequest);
+      });
+
+      it("should call the validation endpoint with correct parameter", () => {
+        expect(axios.get).toHaveBeenCalledWith("mock-token-validation-url", {
+          params: { id_token: "mock-token" }
+        });
+      });
+
+      it("should return the correct user", () => {
+        expect(returnedAuthId).toEqual("mock-sub");
+      });
+    });
+
+    describe("and is NOT successful", () => {
+      const expectedError = "mock-error";
+
+      beforeEach(async () => {
+        axios.get = jest.fn().mockRejectedValue(expectedError);
+        authService = new AuthService();
+        returnedAuthId = await authService.getAuthId(mockRequest);
+      });
+
+      it("should call the validation endpoint with correct parameter", () => {
+        expect(axios.get).toHaveBeenCalledWith("mock-token-validation-url", {
+          params: { id_token: "mock-token" }
+        });
+      });
+
+      it("should return the correct user", () => {
+        expect(returnedAuthId).toEqual(undefined);
+      });
+
+      it("should log correct messages to the console", () => {
+        expect(console.log).toHaveBeenCalledWith("ERROR: AuthService");
+        expect(console.log).toHaveBeenCalledWith(expectedError);
+      });
+    });
   });
 });
