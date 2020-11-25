c66926860bde844cf27797868efa79b758846b85
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-11-24 21:49:10 -0500

    Adding in transaction put handler

---
+++ src/handlers/transactions-put-handler/transactions-put-handler.spec.ts
@@ -0,0 +1,161 @@
+import { APIGatewayProxyResult, Callback } from "aws-lambda";
+import { handler } from "./index";
+import { errorLogger } from "../../utils/error-logger";
+import { responseBodyBuilder } from "../../utils/response-body-builder";
+import { rawTransactions } from "../../mocks/raw-transactions";
+import { transactions } from "../../mocks/transactions";
+
+let mockGetAuthId: jest.Mock;
+let mockUpdateTransaction: jest.Mock;
+
+jest.mock("../../services/auth-service", () => ({
+  AuthService: jest.fn(() => ({
+    getAuthId: mockGetAuthId
+  }))
+}));
+
+jest.mock("../../services/transaction-service", () => ({
+  TransactionService: jest.fn(() => ({
+    updateTransaction: mockUpdateTransaction
+  }))
+}));
+
+jest.mock("../../utils/response-body-builder", () => ({
+  responseBodyBuilder: jest.fn(() => "mock-body-built-response")
+}));
+
+jest.mock("../../utils/error-logger", () => ({
+  errorLogger: jest.fn().mockReturnThis()
+}));
+
+describe("Handlers/Transactions:Put", () => {
+  let callback: Callback<APIGatewayProxyResult>;
+  let event: any;
+
+  beforeEach(async () => {
+    callback = jest.fn();
+  });
+
+  describe("when a transaction is to be updated", () => {
+    describe("and when authentication is NOT successful", () => {
+      const expectedResponse = {
+        statusCode: 401,
+        body: "Unauthorized"
+      };
+
+      beforeEach(async () => {
+        mockGetAuthId = jest.fn().mockRejectedValue("mock-error");
+        await handler(event, undefined, callback);
+      });
+
+      it("should verify the request correctly", () => {
+        expect(mockGetAuthId).toHaveBeenCalledWith(event);
+      });
+
+      it("should log error messages correctly", () => {
+        expect(errorLogger).toHaveBeenCalledWith(
+          "Handler/Transactions:Put",
+          "mock-error"
+        );
+      });
+
+      it("should return the correct response", () => {
+        expect(responseBodyBuilder).toHaveBeenCalledWith(expectedResponse);
+      });
+
+      it("should invoke the callback correctly", () => {
+        expect(callback).toHaveBeenCalledWith(null, "mock-body-built-response");
+      });
+    });
+
+    describe("and when authentication is successful", () => {
+      beforeEach(() => {
+        mockGetAuthId = jest.fn().mockResolvedValue("mock-auth-id");
+      });
+
+      describe("and when request is NOT valid", () => {
+        beforeEach(async () => {
+          await handler(event, undefined, callback);
+        });
+
+        it("should return the correct response", () => {
+          expect(responseBodyBuilder).toHaveBeenCalledWith({
+            statusCode: 400,
+            body: "Bad request"
+          });
+        });
+
+        it("should invoke the callback correctly", () => {
+          expect(callback).toHaveBeenCalledWith(
+            null,
+            "mock-body-built-response"
+          );
+        });
+      });
+
+      describe("and when request is valid", () => {
+        beforeEach(() => {
+          event = {
+            body: JSON.stringify(rawTransactions[0])
+          };
+        });
+
+        describe("and when transaction is updated", () => {
+          beforeEach(async () => {
+            mockUpdateTransaction = jest.fn().mockResolvedValue(undefined);
+            await handler(event, undefined, callback);
+          });
+
+          it("should attempt to update transaction correctly", async () => {
+            expect(mockUpdateTransaction).toHaveBeenCalledWith(
+              "mock-auth-id",
+              rawTransactions[0]
+            );
+          });
+
+          it("should return the correct response", () => {
+            expect(responseBodyBuilder).toHaveBeenCalledWith({
+              statusCode: 200,
+              body: transactions[0]
+            });
+          });
+
+          it("should invoke the callback correctly", () => {
+            expect(callback).toHaveBeenCalledWith(
+              null,
+              "mock-body-built-response"
+            );
+          });
+        });
+
+        describe("and when transaction is NOT updated", () => {
+          beforeEach(async () => {
+            mockUpdateTransaction = jest.fn().mockRejectedValue(undefined);
+            await handler(event, undefined, callback);
+          });
+
+          it("should attempt to update transaction correctly", async () => {
+            expect(mockUpdateTransaction).toHaveBeenCalledWith(
+              "mock-auth-id",
+              rawTransactions[0]
+            );
+          });
+
+          it("should return the correct response", () => {
+            expect(responseBodyBuilder).toHaveBeenCalledWith({
+              statusCode: 500,
+              body: undefined
+            });
+          });
+
+          it("should invoke the callback correctly", () => {
+            expect(callback).toHaveBeenCalledWith(
+              null,
+              "mock-body-built-response"
+            );
+          });
+        });
+      });
+    });
+  });
+});
---
+++ src/handlers/transactions-put-handler/transactions-put-handler.ts
@@ -0,0 +1,72 @@
+import {
+  APIGatewayProxyHandler,
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Callback,
+  Context
+} from "aws-lambda";
+import { HandlerResponse } from "../../models/handler-response";
+import { Transaction } from "../../models/transaction";
+import { AuthService } from "../../services/auth-service";
+import { TransactionService } from "../../services/transaction-service";
+import { errorLogger } from "../../utils/error-logger";
+import { responseBodyBuilder } from "../../utils/response-body-builder";
+
+const transactionsPut: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent,
+  _context: Context,
+  callback: Callback<APIGatewayProxyResult>
+): Promise<APIGatewayProxyResult> => {
+  let authId: string;
+
+  try {
+    const authService = new AuthService();
+    authId = await authService.getAuthId(event);
+  } catch (error) {
+    errorLogger("Handler/Transactions:Put", error);
+    const response: HandlerResponse = responseBodyBuilder({
+      statusCode: 401,
+      body: "Unauthorized"
+    });
+
+    callback(null, response);
+    return;
+  }
+
+  let transaction: Transaction;
+  try {
+    const { body: rawBody } = event;
+    const body = JSON.parse(rawBody);
+    transaction = new Transaction({
+      ...body
+    });
+  } catch (error) {
+    errorLogger("Handler/Transactions:Put", error);
+    const response: HandlerResponse = responseBodyBuilder({
+      statusCode: 400,
+      body: "Bad request"
+    });
+    callback(null, response);
+    return;
+  }
+
+  try {
+    const transactionService = new TransactionService();
+    await transactionService.updateTransaction(authId, transaction);
+
+    const response: HandlerResponse = responseBodyBuilder({
+      statusCode: 200,
+      body: transaction
+    });
+
+    callback(null, response);
+  } catch (error) {
+    const response: HandlerResponse = responseBodyBuilder({
+      statusCode: 500,
+      body: undefined
+    });
+    callback(null, response);
+  }
+};
+
+export { transactionsPut };
