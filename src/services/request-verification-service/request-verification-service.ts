9ce9bec16281189ccfd890af3dbaed96c6cca775
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-11-25 21:15:58 -0500

    Adding in the changes for request verification service and transactions

--- src/services/request-verification-service/request-verification-service.ts
+++ src/services/request-verification-service/request-verification-service.ts
@@ -1,3 +1,23 @@
-class RequestVerificationService {}
+import { Transaction } from "../../models/transaction";
+
+class RequestVerificationService {
+  verifyTransaction = (transaction: Transaction): void => {
+    const hasEmptyAttributes = Object.values(transaction).some(
+      (x) => x === null || x === undefined
+    );
+
+    if (hasEmptyAttributes) {
+      throw new Error("Transaction has undefined attributes");
+    }
+
+    const hasEmptyArrays = Object.values(transaction).some(
+      (x) => Array.isArray(x) && x.length === 0
+    );
+
+    if (hasEmptyArrays) {
+      throw new Error("Transaction has empty attribute arrays");
+    }
+  };
+}
 
 export { RequestVerificationService };
