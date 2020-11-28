9d5c7a473d4ebabcc88060d1bf13927ce650f7e4
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-11-28 14:42:15 -0500

    User put handler working with authorizer

--- src/handlers/user-put-handler/user-put-handler.spec.ts
+++ src/handlers/user-put-handler/user-put-handler.spec.ts
@@ -1,18 +1,12 @@
 import { APIGatewayProxyResult, Callback } from "aws-lambda";
 import { handler } from "./index";
+import { rawUserDetails } from "../../mocks/raw-user-details";
+import * as authIdUtil from "../../utils/auth-id-util";
 import { errorLogger } from "../../utils/error-logger";
 import { responseBodyBuilder } from "../../utils/response-body-builder";
-import { rawUserDetails } from "../../mocks/raw-user-details";
 
-let mockGetAuthId: jest.Mock;
 let mockUpdateUser: jest.Mock;
 let mockVerifyUser: jest.Mock;
-
-jest.mock("../../services/auth-service", () => ({
-  AuthService: jest.fn(() => ({
-    getAuthId: mockGetAuthId
-  }))
-}));
 
 jest.mock("../../services/request-verification-service", () => ({
   RequestVerificationService: jest.fn(() => ({
@@ -26,22 +20,23 @@ jest.mock("../../services/user-service", () => ({
   }))
 }));
 
+jest.mock("../../utils/error-logger");
+
 jest.mock("../../utils/response-body-builder", () => ({
   responseBodyBuilder: jest.fn(() => "mock-body-built-response")
-}));
-
-jest.mock("../../utils/error-logger", () => ({
-  errorLogger: jest.fn().mockReturnThis()
 }));
 
 describe("Handlers/User:Post", () => {
   let callback: Callback<APIGatewayProxyResult>;
   let event: any;
+  let mockGetAuthId: jest.SpyInstance;
 
   beforeEach(async () => {
     callback = jest.fn();
     mockUpdateUser = jest.fn().mockResolvedValue(undefined);
     mockVerifyUser = jest.fn();
+    mockGetAuthId = jest.spyOn(authIdUtil, "getAuthId");
+    mockGetAuthId.mockImplementation(() => "mock-auth-id");
   });
 
   describe("when a user is to be ypdated", () => {
@@ -52,7 +47,9 @@ describe("Handlers/User:Post", () => {
       };
 
       beforeEach(async () => {
-        mockGetAuthId = jest.fn().mockRejectedValue("mock-error");
+        mockGetAuthId.mockImplementation(() => {
+          throw new Error("mock-error");
+        });
         await handler(event, undefined, callback);
       });
 
@@ -63,7 +60,7 @@ describe("Handlers/User:Post", () => {
       it("should log error messages correctly", () => {
         expect(errorLogger).toHaveBeenCalledWith(
           "Handler/User:Put",
-          "mock-error"
+          Error("mock-error")
         );
       });
 
@@ -77,10 +74,6 @@ describe("Handlers/User:Post", () => {
     });
 
     describe("and when authentication is successful", () => {
-      beforeEach(() => {
-        mockGetAuthId = jest.fn().mockResolvedValue("mock-auth-id");
-      });
-
       describe("and when request is NOT valid", () => {
         beforeEach(async () => {
           mockVerifyUser = jest.fn(() => {
